c1d6b9065fa46edae20b5824697d6629cf06b880
Icaro <vbsfalcao@gmail.com>
2017-03-06 17:01:42 -0300

    Using JQuery, change menu animation and add fadeIn for links and title

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,18 @@
-var alternador = document.querySelector('.js-chave');
+$(document).ready(function(){
+	entrada(".cabeca__titulo", 1000);
+	entrada(".link--Github", 1000);
+	entrada(".link--Email", 1600);
+	entrada(".link--LinkedIn", 1900);
+	entrada(".link--skype", 2200);
+	entrada(".menu__item", 1000);
 
-alternador.onclick = function() {
-	var menu = document.querySelector('.js-menu');
-	menu.classList.toggle('menu--exibindo');
-}
+	$( ".js-chave" ).click(
+  function() {
+    $( ".js-menu" ).toggle( "menu--exibindo" );
+  });
+
+  function entrada(variavel, tempo) {
+  	$(variavel).hide();
+	$(variavel).fadeIn(tempo);
+  }
+});
