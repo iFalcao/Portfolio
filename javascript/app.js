a5cf033d3741dc25fd5b40855ae7f9e19d838518
Icaro <vbsfalcao@gmail.com>
2017-03-07 00:13:27 -0300

    Add scroll animation for intern links + add jquery file on js folder

--- javascript/app.js
+++ javascript/app.js
@@ -6,13 +6,26 @@ $(document).ready(function(){
 	entrada(".link--skype", 2200);
 	entrada(".menu__item", 1000);
 
+	$('.menu__botao, .botao-topo[href^="#"]').on('click', function(e) {
+		e.preventDefault();
+		var id = $(this).attr('href'),
+		targetOffset = $(id).offset().top,
+		menuHeight = $('.layout-cabeca').innerHeight();
+		menuHeight = menuHeight < 70 ? 60 : 0;
+
+		$('html, body').animate({ 
+		scrollTop: targetOffset - menuHeight
+		}, 800);
+	});
+
 	$( ".js-chave" ).click(
-  function() {
-    $( ".js-menu" ).toggle( "menu--exibindo" );
-  });
+  		function() {
+    		$( ".js-menu" ).toggle( "menu--exibindo" );
+ 	});
 
   function entrada(variavel, tempo) {
   	$(variavel).hide();
 	$(variavel).fadeIn(tempo);
   }
+
 });
